a277d82e4430929b53e9bf2a8a9cd648a33c11a0
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2024-01-05 18:51:13 -0500

    stoped at video 39

--- BookList/src/index.js
+++ BookList/src/index.js
@@ -7,55 +7,67 @@ const books = [
     author: "Marie Kondo",
     title: "The life-changing magic of tidying up",
     img: "./images/cleaningBook.jpg",
+    id: 1,
   },
   {
     author: "Katerina Lengold",
     title: "Prosto Kosmos",
     img: "./images/prostoKosmos.jpg",
+    id: 2,
   },
 ];
 
-const names = ['john','peter','susan'];
-const newNames = names.map((something)=> {
-  return <h1>{something}</h1>;
-});
-console.log(newNames);
 const BookList = () => {
-  return <section className='booklist'>{newNames}</section>;
-};
-  const Book = (props) => {
-    const { img, title, author } = props;
-    console.log(props);
-    return (
-      <article className="book">
-        <img src={img} alt={title} />
-        <h2> {title} </h2>
-        <h4>{author}</h4>
-      </article>
-    );
-  };
-  /*
   return (
-    <view>
-      <h1>My library</h1>
-      <section className="bookList">
-        <Book
-          author={firstbook.author}
-          title={firstbook.title}
-          img={firstbook.img}
-        >
-          <p>Lorem, ipsum</p>
-          <button>Click me</button>
-        </Book>
-        <Book
-          author={secondbook.author}
-          title={secondbook.title}
-          img={secondbook.img}
-        />
-      </section>
-    </view>
+    <section className="bookList">
+      <EventExamples />
+      {books.map((book) => {
+        return <Book book={book} key={book.id} />;
+      })}
+    </section>
   );
 };
-  */
+
+const EventExamples = () => {
+  const handleFormInput = (e) => {
+    console.log(e.target);
+    console.log(e.target.name);
+    console.log(e.target.value);
+    console.log("handle from input");
+  };
+  const handleButtonClick = () => {
+    alert("handle button click");
+  };
+  const hadleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
+  return (
+    <section>
+      <form onSubmit={hadleFormSubmission}>
+        <h2>Typical form</h2>
+        <input
+          type='text'
+          name='example'
+          onChange={handleFormInput}
+          style={{ margin: "1rem 0" }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
+    </section>
+  );
+};
+const Book = (props) => {
+  const { img, title, author } = props.book;
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2> {title} </h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
