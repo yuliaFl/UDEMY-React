ef46e370584a64920f8ca61c38770a270dd0c377
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2024-01-17 14:10:56 -0500

    complete section 3:

---
+++ BookList/src/books.js
@@ -0,0 +1,24 @@
+import prostoKosmos from './images/prostoKosmos.jpg';
+import cleaningBook from './images/cleaningBook.jpg';
+import HP from './images/HP.jpg';
+
+export const books = [
+    {
+      author: "Marie Kondo",
+      title: "The life-changing magic of tidying up",
+      img: cleaningBook,
+      id: 1,
+    },
+    {
+      author: "Katerina Lengold",
+      title: "Prosto Kosmos",
+      img: prostoKosmos,
+      id: 2,
+    },
+    {
+        author: "J.K. Rowling ",
+        title: "Harry Potter and the Philosopher's Stone",
+        img: HP,
+        id: 3,
+      },
+  ];
--- BookList/src/index.js
+++ BookList/src/index.js
@@ -1,47 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-const books = [
-  {
-    author: "Marie Kondo",
-    title: "The life-changing magic of tidying up",
-    img: "./images/cleaningBook.jpg",
-    id: 1,
-  },
-  {
-    author: "Katerina Lengold",
-    title: "Prosto Kosmos",
-    img: "./images/prostoKosmos.jpg",
-    id: 2,
-  },
-];
+import {books} from './books';
+import Book from './Book';
 
 const BookList = () => {
-  const someValue = "shakeAndBake";
-  const displayValue = () => {
-    console.log(someValue);
-  };
   return (
+    <>
+    <h1>Booksies</h1>
     <section className='bookList'>
-      {books.map((book) => {
-        return <Book book={book} key={book.id} 
-        displayValue={displayValue}/>;
+      {books.map((book,index) => {
+        return <Book {...book} key={book.id} number={index}/>;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author,displayValue } = props.book;
-  //console.log(props);
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2> {title} </h2>
-      <button onClick={displayValue}>Click me</button>
-      <h4>{author}</h4>
-    </article>
+    </>
   );
 };
 
