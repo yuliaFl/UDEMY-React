8135e76f5a51d539c6b2b9b20a94457222e17a15
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2024-01-22 09:51:47 -0500

    stopped at video 61

--- backroads-app/src/App.js
+++ backroads-app/src/App.js
@@ -1,7 +1,20 @@
+import About from './components/about';
+import Hero from './components/hero';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import Services from './components/services';
+import Tours from './components/tours';
 
 function App() {
   return (
-    <h1>hi</h1>
+    <>
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Services/>
+    <Tours/>
+    <Footer/>
+    </>
   );
 }
 
